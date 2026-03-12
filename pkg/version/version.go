d585e0582666ab8eaa7df43b09c0847bd0fbc0c9
ha1tch <bitdrome@gmail.com>
2026-03-12 00:48:06 -0300

    v0.9.7-patched74

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -6,4 +6,4 @@ package version
 
 // Version is the current version of olu.
 // This MUST match the contents of the VERSION file.
-const Version = "0.9.7-patched73"
+const Version = "0.9.7-patched74"
